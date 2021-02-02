706f9a83eefb901bf4b3c2e0e2f0274d6620d37d
INNERJOINT <hykinnerjoint@gmail.com>
2021-02-03 02:36:03 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,5 @@
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall
+./scripts/feeds update -a
+./scripts/feeds install -a
